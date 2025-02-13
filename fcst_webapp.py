d004c02fe2817386cfc5f1a3bdde7880c7e2e060
Japhet Hernández-Vaquero <123884173+JaphetHerzVaq@users.noreply.github.com>
2025-02-12 20:21:22 -0600

    Update fcst_webapp.py

--- fcst_webapp.py
+++ fcst_webapp.py
@@ -135,4 +135,5 @@ def index():
     return render_template("index.html", predictions=predictions, ticker=ticker, image_url=image_url)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
+
